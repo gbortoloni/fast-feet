eb575fafc8bfab565d6a30ca11b6ee1228ae6742
Giuliano Bortoloni <giulianoagria@hotmail.com>
2020-01-31 14:02:59 -0300

    feat(backend): add recipient controller, migration,model

---
+++ backend/src/app/controllers/RecipientController.js
@@ -0,0 +1,101 @@
+import * as Yup from 'yup';
+
+import Recipient from '../models/Recipient';
+
+class RecipientController {
+    async index(req, res) {
+        const recipients = await Recipient.findAll();
+
+        return res.json(recipients);
+    }
+
+    async store(req, res) {
+        const schema = Yup.object().shape({
+            name: Yup.string().required(),
+            rua: Yup.string().required(),
+            numero: Yup.string().required(),
+            complemento: Yup.string(),
+            estado: Yup.string().required(),
+            cidade: Yup.string().required(),
+            cep: Yup.string()
+                .required()
+                .min(9)
+                .max(9),
+        });
+
+        if (!(await schema.isValid(req.body))) {
+            return res.status(400).json({ error: 'Validation falied' });
+        }
+
+        const {
+            id,
+            name,
+            rua,
+            numero,
+            complemento,
+            estado,
+            cidade,
+            cep,
+        } = await Recipient.create(req.body);
+
+        return res.json({
+            id,
+            name,
+            rua,
+            numero,
+            complemento,
+            estado,
+            cidade,
+            cep,
+        });
+    }
+
+    async update(req, res) {
+        const schema = Yup.object().shape({
+            name: Yup.string(),
+            rua: Yup.string(),
+            numero: Yup.string(),
+            complemento: Yup.string(),
+            estado: Yup.string(),
+            cidade: Yup.string(),
+            cep: Yup.string()
+                .min(9)
+                .max(9),
+        });
+
+        if (!(await schema.isValid(req.body))) {
+            return res.status(400).json({ error: 'Validation falied' });
+        }
+
+        const { id } = req.params;
+
+        const recipient = await Recipient.findByPk(id);
+
+        if (!recipient) {
+            return res.status(401).json({ error: 'Recipient does not exists' });
+        }
+
+        const {
+            name,
+            rua,
+            numero,
+            complemento,
+            estado,
+            cidade,
+            cep,
+        } = await recipient.update(req.body);
+
+        return res.json({
+            id,
+            name,
+            rua,
+            numero,
+            complemento,
+            estado,
+            cidade,
+            cep,
+        });
+    }
+}
+
+export default new RecipientController();
--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,6 +1,7 @@
 import { Router } from 'express';
 
 import SessionController from './app/controllers/SessionController';
+import RecipientController from './app/controllers/RecipientController';
 
 import authMiddleware from './app/middlewares/auth';
 
@@ -9,5 +10,9 @@ const routes = new Router();
 routes.post('/sessions', SessionController.store);
 
 routes.use(authMiddleware);
+
+routes.get('/recipients', RecipientController.index);
+routes.post('/recipients', RecipientController.store);
+routes.put('/recipients/:id', RecipientController.update);
 
 export default routes;
